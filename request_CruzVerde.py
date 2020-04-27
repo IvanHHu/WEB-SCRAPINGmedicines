67977a4fc5ff2a207f97fc92ed4686aadcdee813
Ivan Huerta Hernández <ivanlgzone@gmail.com>
2020-04-26 22:50:57 -0500

    Estatus code integration
    Se agregaron estatus code para la comunicación con Angular

--- request_CruzVerde.py
+++ request_CruzVerde.py
@@ -58,81 +58,96 @@ def main(medicamento):
     #------------------------------------------------------------------------------------------------------
     url = 'https://www.cruzverde.com.co/search?q=' + medicamento + "&search-button=&lang=es_CO"
     
-    pagina = s.get(url, proxies={"http": proxy},headers=headers, timeout=5)
-    print(url, proxies, headers)
     try:
+        pagina = s.get(url, proxies={"http": proxy},headers=headers, timeout=5)
+        print(url, proxies, headers)
         if pagina.status_code == 200:
-            pagina.encoding = 'ISO-8859-1'
-            txtHtml = html.fromstring(pagina.content)
-            urlPAGS = txtHtml.xpath("//span[@class='text-uppercase pagination-text']/text()")
-            fichaTec = txtHtml.xpath("//span[@class='tab1Title']/text()")
-            sinResultados = txtHtml.xpath("//p[@class='no-results-message text-center']/text()")
-            pagina = 0
-            #print(sinResultados)
             try:
+                pagina.encoding = 'ISO-8859-1'
+                txtHtml = html.fromstring(pagina.content)
+                urlPAGS = txtHtml.xpath("//span[@class='text-uppercase pagination-text']/text()")
+                fichaTec = txtHtml.xpath("//span[@class='tab1Title']/text()")
+                sinResultados = txtHtml.xpath("//p[@class='no-results-message text-center']/text()")
+                pagina = 0
+                #print(sinResultados)
                 if sinResultados == []:
                     #Cuando hay varias paginas
                     if urlPAGS != [] :
-                        for pagina in range(50):
-                            print(pagina + 1)
-                            pagina= pagina*12  
-                            url2 = 'https://www.cruzverde.com.co/search?q=' + medicamento + "&search-button=&lang=es_CO&start=" + str(pagina) +"&sz=12"
-                            #print(url2)
-                            pagina2 = s.get(url2, proxies={"http": proxy },headers=headers, timeout=5)
-                            print(url2, proxies, headers)
-                            txtHtml2 = html.fromstring(pagina2.content)
-                            sinResultados2 = txtHtml2.xpath("//p[@class='no-results-message text-center']/text()")
-                            #print(sinResultados2)
-                            #print(url2)
+                        try:
+                            for pagina in range(50):
+                                print(pagina + 1)
+                                pagina= pagina*12  
+                                url2 = 'https://www.cruzverde.com.co/search?q=' + medicamento + "&search-button=&lang=es_CO&start=" + str(pagina) +"&sz=12"
+                                #print(url2)
+                                pagina2 = s.get(url2, proxies={"http": proxy },headers=headers, timeout=5)
+                                print(url2, proxies, headers)
+                                txtHtml2 = html.fromstring(pagina2.content)
+                                sinResultados2 = txtHtml2.xpath("//p[@class='no-results-message text-center']/text()")
+                                #print(sinResultados2)
+                                #print(url2)
 
-                            if sinResultados2 == []:
-                                nombres = txtHtml2.xpath("//a[@class='link']/text()")
-                                precios = txtHtml2.xpath("//span[@class='value pr-2']/text()")
-                                for n,p in zip(nombres,precios):
-                                    n= n.replace("\n        \n            ", "")
-                                    n= n.replace("\n        \n    ", "")
-                                    p= p.replace("\n                    ", "")
-                                    p= p.replace("\n                ", "")
-                                    print(n +":"+p) 
-                            else :
-                                break    
-
+                                if sinResultados2 == []:
+                                    nombres = txtHtml2.xpath("//a[@class='link']/text()")
+                                    precios = txtHtml2.xpath("//span[@class='value pr-2']/text()")
+                                    for n,p in zip(nombres,precios):
+                                        n= n.replace("\n        \n            ", "")
+                                        n= n.replace("\n        \n    ", "")
+                                        p= p.replace("\n                    ", "")
+                                        p= p.replace("\n                ", "")
+                                        print(n +":"+p) 
+                                else :
+                                    break
+                            return 0
+                        except:
+                            print("La request tiene bastantes productos y fallo en esta sección")
+                            return -3
                     #cuando solo hay un medicamnto
                     elif fichaTec != []:
-
-                        nombres = txtHtml.xpath("//h1[@class='product-name']/text()")
-                        precios = txtHtml.xpath("//span[@class='value pr-2']/text()")
-                        nombres[0] =nombres[0].replace("\n        \n            ", "")
-                        nombres[0] =nombres[0].replace("\n        \n    ", "")
-                        precios[0] = precios[0].replace("\n                    ", "")
-                        precios[0] = precios[0].replace("\n                ", "")
-                
-                        print(nombres[0] + ":"+ precios[0] )
-
+                        try:
+                            nombres = txtHtml.xpath("//h1[@class='product-name']/text()")
+                            precios = txtHtml.xpath("//span[@class='value pr-2']/text()")
+                            nombres[0] =nombres[0].replace("\n        \n            ", "")
+                            nombres[0] =nombres[0].replace("\n        \n    ", "")
+                            precios[0] = precios[0].replace("\n                    ", "")
+                            precios[0] = precios[0].replace("\n                ", "")
+                    
+                            print(nombres[0] + ":"+ precios[0] )
+                            return 0
+                        except:
+                            print("La request tiene tiene entre 2 y 12 productos y fallo en esta sección")
+                            return -2
                     #cuando hay solo una pagina
                     elif fichaTec == []:
-                        nombres = txtHtml.xpath("//a[@class='link']/text()")
-                        precios = txtHtml.xpath("//span[@class='value pr-2']/text()")
-                        for n,p in zip(nombres,precios):
-                            n= n.replace("\n        \n            ", "")
-                            n= n.replace("\n        \n    ", "")
-                            p= p.replace("\n                    ", "")
-                            p= p.replace("\n                ", "")
-                            print(n +":"+p)
+                        try:
+                            nombres = txtHtml.xpath("//a[@class='link']/text()")
+                            precios = txtHtml.xpath("//span[@class='value pr-2']/text()")
+                            for n,p in zip(nombres,precios):
+                                n= n.replace("\n        \n            ", "")
+                                n= n.replace("\n        \n    ", "")
+                                p= p.replace("\n                    ", "")
+                                p= p.replace("\n                ", "")
+                                print(n +":"+p)
+                        except:
+                            print("La request tiene un producto y fallo en esta sección")
+                            return -1
                 else:
                     print(sinResultados[0])
             
             except:
-                print("Error")
+                print("La petición echa no fue exitosa")
+                return 3
                           
         else:
-            print("Error al cargar la pagina")
+            print("El request fue exitoso, mas algo paso en la pagina")
+            return 2
 
 
     except:
-        print("Error al cargar la pagina")
+        print("Error desconocido al iniciar la petición")
+        return 1
 
             
 
 if __name__ == '__main__':
-   main(sys.argv[1])
+  status =  main(sys.argv[1])
+  print(status)
