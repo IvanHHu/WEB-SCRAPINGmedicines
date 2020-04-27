cbbcb75a6186e8aa6982f8d17a88c118754f88a9
aldorojas <64167239+aldorojas@users.noreply.github.com>
2020-04-26 21:14:54 -0500

    update with headers

--- request_CruzVerde.py
+++ request_CruzVerde.py
@@ -4,6 +4,7 @@ from lxml import html
 from lxml.html import fromstring
 from itertools import cycle
 import traceback
+import random
 
 def main(medicamento):
     def get_proxies():
@@ -16,6 +17,39 @@ def main(medicamento):
                 proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
                 proxies.add(proxy)
         return proxies
+    
+    #----------------------------------------------------------------------------------------------------
+    user_agent_list = [
+    #Chrome
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        #Firefox
+        'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+    ]
+    user_agent = random.choice(user_agent_list)
+    #Set the headers 
+    headers = {'User-Agent': user_agent}
+
     #-----------------------------------------------------------------------------------------------------
     proxies = get_proxies()
     proxy_pool = cycle(proxies)
@@ -24,8 +58,8 @@ def main(medicamento):
     #------------------------------------------------------------------------------------------------------
     url = 'https://www.cruzverde.com.co/search?q=' + medicamento + "&search-button=&lang=es_CO"
     
-    pagina = s.get(url, proxies={"http": proxy}, timeout=5)
-    print(url, proxies)
+    pagina = s.get(url, proxies={"http": proxy},headers=headers, timeout=5)
+    print(url, proxies, headers)
     try:
         if pagina.status_code == 200:
             pagina.encoding = 'ISO-8859-1'
@@ -44,8 +78,8 @@ def main(medicamento):
                             pagina= pagina*12  
                             url2 = 'https://www.cruzverde.com.co/search?q=' + medicamento + "&search-button=&lang=es_CO&start=" + str(pagina) +"&sz=12"
                             #print(url2)
-                            pagina2 = s.get(url2, proxies={"http": proxy }, timeout=5)
-                            print(url2, proxies)
+                            pagina2 = s.get(url2, proxies={"http": proxy },headers=headers, timeout=5)
+                            print(url2, proxies, headers)
                             txtHtml2 = html.fromstring(pagina2.content)
                             sinResultados2 = txtHtml2.xpath("//p[@class='no-results-message text-center']/text()")
                             #print(sinResultados2)
@@ -102,5 +136,3 @@ def main(medicamento):
 
 if __name__ == '__main__':
    main(sys.argv[1])
-
-
